55d614c019f3df78e65b98b6269edcb2ca683129
Daniel Jackson <email@danieljackson.co.uk>
2022-05-18 13:43:55 +0000

    Fix button/back going to wrong screen if info app launched from quick launcher / settings screens.

--- src/displayapp/screens/settings/Settings.cpp
+++ src/displayapp/screens/settings/Settings.cpp
@@ -57,7 +57,7 @@ std::unique_ptr<Screen> Settings::CreateScreen2() {
 #elif defined(CUEBAND_APP_ENABLED)
     {CUEBAND_APP_SYMBOL, "Cues", Apps::CueBand},
 #elif defined(CUEBAND_INFO_APP_ENABLED)
-    {CUEBAND_INFO_APP_SYMBOL, "Info", Apps::Info},
+    {CUEBAND_INFO_APP_SYMBOL, "Info", Apps::InfoFromSettings},
 #else
     {Symbols::none, "None", Apps::None},
 #endif
@@ -85,7 +85,7 @@ std::unique_ptr<Screen> Settings::CreateScreen4() {
   std::array<Screens::List::Applications, 4> applications {{
     {Symbols::list, "About", Apps::SysInfo},
 #if (!defined(CUEBAND_CUSTOMIZATION_NO_STEPS) || defined(CUEBAND_OPTIONS_APP_ENABLED) || defined(CUEBAND_APP_ENABLED)) && defined(CUEBAND_INFO_APP_ENABLED)
-    {CUEBAND_INFO_APP_SYMBOL, "Info", Apps::Info},
+    {CUEBAND_INFO_APP_SYMBOL, "Info", Apps::InfoFromSettings},
 #else
     {Symbols::none, "None", Apps::None},
 #endif
