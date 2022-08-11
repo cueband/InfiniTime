71202dce25fa23a60111d25362e3aab4a6b06054
Daniel Jackson <email@danieljackson.co.uk>
2022-08-11 15:49:54 +0000

    -

--- src/cueband.local.h
+++ src/cueband.local.h
@@ -1,4 +1,5 @@
 #include "cueband.local.example.h"
 
-// Test build: entirely disable app launcher
+// Test build: entirely disable app launcher and notifications
 #define CUEBAND_DISABLE_APP_LAUNCHER
+#define CUEBAND_DISABLE_NOTIFICATIONS
